c490bc89ab5d71dcaf442748d18e6c5f6726e1f4
Shruthi h <shruthi.h@inmobi.com>
2022-01-17 09:01:22 +0530

    jest-enzyme setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import {shallow} from 'enzyme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  const component = shallow(<App />);
+  expect(component.find('.App-header').text()).toEqual('Edit src/App.js and save to reload.Learn React');
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
